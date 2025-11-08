d19ec7d81d2f33d62d2868c41b3c59a1e04b2f34
Ilia Loviagin <ilia.loviagin@gmail.com>
2025-11-08 20:55:11 +0300

    Added pre-generate dynamic parameters for /profile/[uid]

--- app/profile/[nickname]/page.tsx
+++ app/profile/[nickname]/page.tsx
@@ -1,15 +1,15 @@
 
 import UserProfilePage from './components/UserProfilePage/UserProfilePage';
 
-// export async function generateStaticParams() {
-//     const base = process.env.NEXT_PUBLIC_URL!;
-//     const response = await fetch(`${base}/api/users/nicknames`);
-//     const data = await response.json();
-//     const nicknames = data["nicknames"];
-//     return (nicknames as string[])
-//         .filter(n => typeof n === "string" && n.length > 0)
-//         .map(nickname => ({ nickname }));
-// }
+export async function generateStaticParams() {
+    const base = process.env.NEXT_PUBLIC_URL!;
+    const response = await fetch(`${base}/api/users/nicknames`);
+    const data = await response.json();
+    const nicknames = data["nicknames"];
+    return (nicknames as string[])
+        .filter(n => typeof n === "string" && n.length > 0)
+        .map(nickname => ({ nickname }));
+}
 
 export default async function UserProfile({ params }: { params: Promise<{ nickname: string }> }) {
     const { nickname } = await params;
