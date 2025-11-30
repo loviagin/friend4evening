cd923794a0ab53e338cac2d5a71228161fc177f7
Ilia Loviagin <ilia.loviagin@gmail.com>
2025-12-01 01:31:30 +0300

    Added robots

--- app/account/meets/[uid]/page.tsx
+++ app/account/meets/[uid]/page.tsx
@@ -2,11 +2,19 @@ import { collection, getDocs, query, where } from 'firebase/firestore';
 import MeetContent from './components/MeetContent/MeetContent';
 import styles from './page.module.css'
 import { db } from '@/lib/firebase';
+import { Metadata } from 'next';
 
 export async function generateStaticParams() {
     const q = query(collection(db, "meets"), where("type", "==", "open"))
     const snap = await getDocs(q);
     return snap.docs.map(d => ({ id: d.id }));
+}
+
+export const metadata: Metadata = {
+    robots: {
+    index: false,
+    follow: false
+  }
 }
 
 export default async function Meet({ params }: { params: Promise<{ uid: string }> }) {
